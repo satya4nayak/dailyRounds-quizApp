d9624c0c3e92a06b0fbff684ade18a61d89538e4
satyanayak <satyanayak289@gmail.com>
2026-07-10 19:28:52 +0530

    chore: add .gitignore and untrack build artifacts, .idea, local.properties

--- feature/quiz/build/tmp/kapt3/stubs/debug/com/assignment/mcqquiz/feature/quiz/ui/component/StreakBadgeKt.java
+++
@@ -1,9 +0,0 @@
-package com.assignment.mcqquiz.feature.quiz.ui.component;
-
-@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\u000e\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\b\n\u0000\u001a\u0010\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u0003H\u0007\u00a8\u0006\u0004"}, d2 = {"StreakBadge", "", "currentStreak", "", "quiz_debug"})
-public final class StreakBadgeKt {
-    
-    @androidx.compose.runtime.Composable()
-    public static final void StreakBadge(int currentStreak) {
-    }
-}
